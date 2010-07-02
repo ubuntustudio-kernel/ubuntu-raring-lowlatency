12b2af268c40f8fe969433b3caf4b4726cc03d48
Andy Whitcroft <apw@canonical.com>
2010-07-02 18:45:50 +0100

    UBUNTU: ubuntu: AUFS
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Signed-off-by: Leann Ogasawara <leann.ogasawara@canonical.com>

--- fs/namespace.c
+++ fs/namespace.c
@@ -1419,6 +1419,7 @@ int iterate_mounts(int (*f)(struct vfsmount *, void *), void *arg,
 	}
 	return 0;
 }
+EXPORT_SYMBOL(iterate_mounts);
 
 static void cleanup_group_ids(struct mount *mnt, struct mount *end)
 {
