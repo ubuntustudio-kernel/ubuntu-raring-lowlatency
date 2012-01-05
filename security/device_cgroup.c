2c1a8ff0ddb29d60d45ab4317bfc22f2e8e3411f
Andy Whitcroft <apw@canonical.com>
2012-01-05 14:28:31 +0000

    UBUNTU: ubuntu: AUFS -- aufs3-standalone.patch
    Signed-off-by: Andy Whitcroft <apw@canonical.com>

--- security/device_cgroup.c
+++ security/device_cgroup.c
@@ -573,6 +573,7 @@ int __devcgroup_inode_permission(struct inode *inode, int mask)
 	return __devcgroup_check_permission(dev_cgroup, type, imajor(inode),
 					    iminor(inode), access);
 }
+EXPORT_SYMBOL(__devcgroup_inode_permission);
 
 int devcgroup_inode_mknod(int mode, dev_t dev)
 {
