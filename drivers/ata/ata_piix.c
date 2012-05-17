3b9b2698c1d1a041c60364ba1318398e433e3b2d
Andy Whitcroft <apw@canonical.com>
2012-05-17 18:55:28 +0100

    UBUNTU: SAUCE: ata_piix: add a disable_driver option
    We need to be able to fully disable the ata_piix driver from the kernel
    command line.  Add a generic disable_driver to the module init to
    completely short circuit the initialisation.
    
    BugLink: http://bugs.launchpad.net/bugs/994870
    Signed-off-by: Andy Whitcroft <apw@canonical.com>
    Acked-by: Luis Henriques <luis.henriques@canonical.com>
    Acked-by: Herton Krzesinski <herton.krzesinski@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Signed-off-by: Tim Gardner <tim.gardner@canonical.com>
    Signed-off-by: Leann Ogasawara <leann.ogasawara@canonical.com>

--- drivers/ata/ata_piix.c
+++ drivers/ata/ata_piix.c
@@ -1727,9 +1727,15 @@ static void piix_remove_one(struct pci_dev *pdev)
 	ata_pci_remove_one(pdev);
 }
 
+static bool disable_driver = 0;
 static int __init piix_init(void)
 {
 	int rc;
+
+	if (disable_driver) {
+		printk(KERN_WARNING "ata_piix: driver disabled completely\n");
+		return 0;
+	}
 
 	DPRINTK("pci_register_driver\n");
 	rc = pci_register_driver(&piix_pci_driver);
@@ -1747,5 +1753,6 @@ static void __exit piix_exit(void)
 	pci_unregister_driver(&piix_pci_driver);
 }
 
+module_param(disable_driver, bool, 0);
 module_init(piix_init);
 module_exit(piix_exit);
