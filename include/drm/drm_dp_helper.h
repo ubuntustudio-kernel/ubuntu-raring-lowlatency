3c2b273ecc6d07a694e13da16a1c8386d4bd9167
Daniel Vetter <daniel.vetter@ffwll.ch>
2012-10-18 10:15:24 +0200

    UBUNTU: SAUCE: i915_hsw: drm: dp helper: extract drm_dp_channel_eq_ok
    BugLink: http://bugs.launchpad.net/bugs/1085245
    
    Only backport relevant function calls for ubuntu/i915 driver.
    
    Reviewed-by: Alex Deucher <alexander.deucher@amd.com>
    Acked-by: Dave Airlie <airlied@gmail.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    
    (backported from commit be2b0fc1bbbf2bd553e54cd1172070b2dfd3dd39 in git://people.freedesktop.org/~danvet/drm backport-3.6)
    Signed-off-by: Leann Ogasawara <leann.ogasawara@canonical.com>
    Acked-by: Brad Figg <brad.figg@canonical.com>
    Acked-by: Tim Gardner <tim.gardner@canonical.com>

--- include/drm/drm_dp_helper.h
+++ include/drm/drm_dp_helper.h
@@ -258,6 +258,10 @@ struct i2c_algo_dp_aux_data {
 int
 i2c_dp_aux_add_bus(struct i2c_adapter *adapter);
 
+#define DP_LINK_STATUS_SIZE       6
+bool drm_dp_channel_eq_ok(u8 link_status[DP_LINK_STATUS_SIZE],
+			  int lane_count);
+
 #define DP_RECEIVER_CAP_SIZE    0xf
 void drm_dp_link_train_clock_recovery_delay(u8 dpcd[DP_RECEIVER_CAP_SIZE]);
 void drm_dp_link_train_channel_eq_delay(u8 dpcd[DP_RECEIVER_CAP_SIZE]);
