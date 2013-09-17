3868aeb1124cdde127b9b19a94327b5df819cbbc
Jani Nikula <jani.nikula@intel.com>
2013-09-17 18:33:43 +0300

    drm/i915: do not update cursor in crtc mode set
    BugLink: http://bugs.launchpad.net/bugs/1240580
    
    commit cc173961a68034c1171a421f0dbed39edfb60880 upstream.
    
    The cursor is disabled before crtc mode set in crtc disable (and we
    assert this is the case), and enabled afterwards in crtc enable. Do not
    update it in crtc mode set.
    
    On HSW enabling a plane on a disabled pipe may hang the entire system.
    And there's no good reason for doing it ever, so just don't.
    
    v2: Add note about HSW hangs - vsyrjala
    
    Suggested-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Reviewed-by: Ville Syrjälä <ville.syrjala@linux.intel.com>
    Signed-off-by: Jani Nikula <jani.nikula@intel.com>
    Tested-by: Paulo Zanoni <paulo.r.zanoni@intel.com>
    Signed-off-by: Daniel Vetter <daniel.vetter@ffwll.ch>
    [ kamal: backport to 3.8 ]
    Signed-off-by: Kamal Mostafa <kamal@canonical.com>
    Signed-off-by: Brad Figg <brad.figg@canonical.com>

--- drivers/gpu/drm/i915/intel_display.c
+++ drivers/gpu/drm/i915/intel_display.c
@@ -4753,9 +4753,6 @@ static int i9xx_crtc_mode_set(struct drm_crtc *crtc,
 		return -EINVAL;
 	}
 
-	/* Ensure that the cursor is valid for the new mode before changing... */
-	intel_crtc_update_cursor(crtc, true);
-
 	if (is_lvds && dev_priv->lvds_downclock_avail) {
 		/*
 		 * Ensure we match the reduced clock's P to the target clock.
@@ -5642,9 +5639,6 @@ static int ironlake_crtc_mode_set(struct drm_crtc *crtc,
 		return -EINVAL;
 	}
 
-	/* Ensure that the cursor is valid for the new mode before changing... */
-	intel_crtc_update_cursor(crtc, true);
-
 	/* determine panel color depth */
 	dither = intel_choose_pipe_bpp_dither(crtc, fb, &intel_crtc->bpp,
 					      adjusted_mode);
@@ -5844,9 +5838,6 @@ static int haswell_crtc_mode_set(struct drm_crtc *crtc,
 			return -EINVAL;
 		}
 	}
-
-	/* Ensure that the cursor is valid for the new mode before changing... */
-	intel_crtc_update_cursor(crtc, true);
 
 	/* determine panel color depth */
 	dither = intel_choose_pipe_bpp_dither(crtc, fb, &intel_crtc->bpp,
